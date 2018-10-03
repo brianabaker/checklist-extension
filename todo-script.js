bdda0a54ec08d38fdf98feed491216363e9ab421
github username <github email address>
2018-10-03 15:08:42 -0400

    stored as an array in chrome storage, can push correctly

--- todo-script.js
+++ todo-script.js
@@ -6,36 +6,85 @@
 // next step:
 // need to be able to add multiple strings and then display.
 
+// okay let's remember how to make a todo list in vanilla JS
+
+chrome.runtime.onStartup.addListener(() => {
+  console.log("I started up!");
+  let defaultValue = [];
+  chrome.storage.sync.get({list: defaultValue}, function(data) {
+    chrome.storage.sync.set({list: data.list}, function() {
+    });
+  });
+});
+
 const textinput = document.getElementById('add-todo-input');
-
 const getButton = document.getElementById('getButton');
+const leftColumn = document.getElementById('left-column');
 
+// this is a temp button to show what's in the get. right now only showing one item.
 getButton.addEventListener('click', (e) => {
+  // this is returning the new array
   tempButton();
 })
 
+// add a todo by pressing enter
 textinput.addEventListener('keypress', (e) => {
+  // if enter key
   if (e.keyCode === 13){
-    saveChanges();
-  }
+    // to append a new todo first needs to get the array from chrome storage
+    // calls the update function with the data in chrome storage
+    chrome.storage.sync.get({ list: [] }, function(data) {
+       console.log(data.list);
+       update(data.list); //storing the storage value in a variable and passing to update function
+     })
+   }
 })
 
+function update(array){
+  let newTodo = textinput.value;
+  array.push(newTodo);
+  console.log('array', array);
+  chrome.storage.sync.set({ list: array }, function() {
+    console.log("added to list with new values");
+  });
+}
+
+
 function tempButton(){
-  chrome.storage.sync.get('value', function (data) {
-    console.log(data)
+  chrome.storage.sync.get('list', function (data) {
+    console.log(data);
+  });
+}
+
+function displayTodos(){
+  const li = document.createElement('li');
+  console.log('in the display todos');
+  chrome.storage.sync.get('list', function (data) {
+    console.log(data);
+    li.innerHTML = data.list;
+    leftColumn.append(li);
   });
 }
 
 function addTodo() {
-  let theValue = textinput.value;
-
+  let newTodo = textinput.value;
   console.log(theValue);
   if (!theValue) {
-    console.log("No value is set")
+    console.log("No value is set");
     return;
   }
+  chrome.storage.sync.set({'list': [newTodo]}, function() {
+    console.log('set to ' + newTodo);
+  });
+  displayTodos();
+}
 
-  chrome.storage.sync.set({'value': theValue}, function() {
-    console.log('set to ' + theValue);
+function updateTodos(array, value) {
+  let newTodo = textinput.value;
+    // add new todo to array of todos
+  array.push(newTodo);
+    // set the updated array in chrome storage
+  chrome.storage.sync.set({'list': array}, function() {
+      console.log("added to list with new values");
   });
 }
